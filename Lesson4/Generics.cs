d21419f927bbbe8bc9497ec02348befd2eb8607b
fyoy <fedorzavaley@gmail.com>
2023-02-15 20:24:05 +0300

    Решение 4-й задачи

---
+++ Lesson4/Generics.cs
@@ -0,0 +1,40 @@
+﻿namespace Lesson4
+{
+    internal class SimpleGenericCache<T>
+    {
+        private readonly Dictionary<string, CachedValue<T>> _cache = new();
+        internal void Store(string key, T value, int timeout = 30)
+        {
+            var CachedValue = new CachedValue<T>
+            {
+                CreationTime = DateTime.Now,
+                Timeout = timeout,
+                Value = value
+            };
+
+            _cache[key] = CachedValue;
+        }
+        internal CachedValue<T>? Fetch(string key)
+        {
+            if (_cache.TryGetValue(key, out var value))
+            {
+                var timeCompare = value.CreationTime + TimeSpan.FromSeconds(value.Timeout);//Время + таймаут
+                if(timeCompare >= DateTime.Now)
+                {
+                    return value;
+                }
+                else
+                {
+                    _cache.Remove(key);
+                }
+            }
+            return default;
+        }
+    }
+    internal record CachedValue<T>
+    {
+        public T? Value { get; init; }
+        public DateTime CreationTime { get; init; } = DateTime.Now;
+        public int Timeout { get; init; }
+    }
+}
---
+++ Lesson4/Program.cs
@@ -0,0 +1,18 @@
+﻿using Lesson4;
+
+var @generic = new SimpleGenericCache<string>();
+var @value = "My mythical value";
+
+@generic.Store("1", @value);
+
+//Выведет данные для значения 1, потому что будет применен 
+//стандартный таймаут в 30 секунд
+var @out = @generic.Fetch("1");
+Console.WriteLine(@out);
+
+//Не выведет данные, потому что к моменту вызова
+//проверки в Fetch DateTime.Now будет больше чем CreationDate
+@generic.Store("2", @value,0);
+
+@out = @generic.Fetch("2");
+Console.WriteLine(@out);
