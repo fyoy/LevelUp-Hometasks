7a61134607b673d804d86fb750f0a8ccdb598975
fyoy <fedorzavaley@gmail.com>
2023-02-23 15:07:14 +0300

    Решение для 5 задачи

---
+++ Lesson5/Program.cs
@@ -0,0 +1,5 @@
+﻿using Lesson5;
+
+var @string = "     \tThis is \n a   test string             123 ";
+
+Console.WriteLine($"Результат: [{@string.Clear()}]");
---
+++ Lesson5/StringExt.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson5
+{
+    public static class StringExt
+    {
+        public static string Clear(this string str)
+        {
+            StringBuilder sb = new StringBuilder();
+
+
+            //Формирование новой строки с содержанием только (a=z,A-Z,0-9)
+            foreach (char c in str)
+            {
+                if ((c >= '0' && c <= '9') || (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || c == ' ')
+                {
+                    //Удаление лишних пробелов в тексте
+                    if (sb.Length > 0)
+                    {
+                        if (!(c == ' ' && c == sb[sb.Length - 1]))
+                        {
+                            sb.Append(c);
+                        }
+                    }
+                    //Первое удаление, когда sb.Length ещё 0
+                    else
+                    {
+                        sb.Append(c);
+                    }
+                }
+            }
+            return sb.ToString().Trim();
+        }
+
+    }
+}
